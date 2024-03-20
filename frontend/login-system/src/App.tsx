cf8b62b92f269b8938598e4a9fc174f856f23ba9
progspace1204 <progspace1204@gmail.com>
2024-03-20 06:40:49 -0700

    create: login system

---
+++ frontend/login-system/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="max-w-[300px]" href="/hello">
+          Login with Facebook
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
